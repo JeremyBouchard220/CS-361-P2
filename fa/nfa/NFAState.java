2ac8440c2481eabe2a28ef58a5fb27e718fc99ca
JeremyBouchard220 <60022660+JeremyBouchard220@users.noreply.github.com>
2021-04-09 16:47:07 -0600

    NFAState Update #1
    Added what should be all of the required methods.

--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -5,5 +5,73 @@ import java.util.*;
 
 public class NFAState extends State
 {
+    private boolean isStart, isFinal;
+    private NFAState previousState;
     
+    public NFAState(String name)
+    {
+        //TODO
+    }
+
+    public NFAState(String name, boolean isStart, boolean isFinal)
+    {
+        //TODO
+    }
+
+    public void setStartState(boolean value)
+    {
+        isStart = value;
+    }
+    
+    public boolean isStartState()
+    {
+        return isStart;
+    }
+
+    public void setFinalState(boolean value)
+    {
+        isFinal = value;
+    }
+    
+    public boolean isFinalState()
+    {
+        return isFinal;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setPreviousState(NFAState oldState)
+    {
+        //TODO
+    }
+
+    public NFAState getPreviousState()
+    {
+        return previousState;
+    }
+    
+    public Set<NFAState> getToState(char symb)
+    {
+        //TODO
+        return null;
+    }
+    
+    private void initDefault(String name)
+    {
+        //TODO
+    }
+
+    public Set<NFAState> getEStates()
+    {
+        //TODO
+        return null;
+    }
+
+    public void addTransition(char onSymb, NFAState toState)
+    {
+        //TODO
+    }
 }
